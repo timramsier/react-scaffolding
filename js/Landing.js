aae1c7ecb748604a996cb58c00f9c715f3eecbb3
techanda <timramsier@gmail.com>
2017-03-22 10:45:10 -0700

    Commit scaffolding

---
+++ js/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react'
+// import { Link } from 'react-router'
+
+const Landing = React.createClass({
+  render () {
+    return (
+      <div className='landing'>
+        <h1>React Scaffolding: React is properly rendering</h1>
+      </div>
+    )
+  }
+})
+
+export default Landing
